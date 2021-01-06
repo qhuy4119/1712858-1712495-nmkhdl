9d00fdd9d3e24783572a616547cba727678303c1
Huy Nguyen Quang <quanghuy4119@gmail.com>
2021-01-06 13:20:36 +0700

    add code to crawl multiple forums
    list of forums to crawl is specified in forumsList.py
    the forum spider will use forums_to_crawl in forumsList.py as its
    start_urls
    add code to print logs while crawling

---
+++ scraping/scraping/forumsList.py
@@ -0,0 +1,4 @@
+forums_to_crawl = [
+    'https://voz.vn/f/xe-cac-loai.77/',
+    'https://voz.vn/f/intel.24/',
+]
--- scraping/scraping/spiders/forumSpider.py
+++ scraping/scraping/spiders/forumSpider.py
@@ -1,19 +1,21 @@
 import scrapy
+from .. import forumsList
 
 class forumSpider(scrapy.Spider):
     name = 'forum'
-    start_urls = ["https://voz.vn/f/xe-cac-loai.77/"]
+    start_urls = forumsList.forums_to_crawl 
 
     #Parse a forum page. Example of a forum: https://voz.vn/f/phan-cung-chung.9/
     def parse(self, response):
         postLinks = response.css("div.structItem-title").xpath(".//a[@class='']")
         yield from response.follow_all(postLinks, callback=self.parse_post)
+        forumTitle = response.css("h1.p-title-value::text").get()
         next_forum_page = response.css("a.pageNav-jump.pageNav-jump--next::attr(href)").get()
         if next_forum_page is not None:
             print('Crawling next forum page. URL: ', response.urljoin(next_forum_page))
             yield response.follow(next_forum_page, callback=self.parse)
         else:
-            print("There's no next page to crawl")
+            print(f"There's no next page of {forumTitle} to crawl")
 
     #Parse a post page. Example of a post: https://voz.vn/t/xiaomi-mi-11-ve-viet-nam-gia-hon-15-trieu-dong.208165/
     def parse_post(self, response):
