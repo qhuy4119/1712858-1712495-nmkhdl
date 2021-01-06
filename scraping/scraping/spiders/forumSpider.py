de9c98ca30f4f9aaa2e4f8cf873f0c3268f96996
Huy Nguyen Quang <quanghuy4119@gmail.com>
2021-01-06 12:13:52 +0700

    add following next page of a forum -> now able to crawl the whole forum

--- scraping/scraping/spiders/forumSpider.py
+++ scraping/scraping/spiders/forumSpider.py
@@ -8,6 +8,10 @@ class forumSpider(scrapy.Spider):
     def parse(self, response):
         postLinks = response.css("div.structItem-title").xpath(".//a[@class='']")
         yield from response.follow_all(postLinks, callback=self.parse_post)
+        next_forum_page = response.css("a.pageNav-jump.pageNav-jump--next::attr(href)").get()
+        if next_forum_page is not None:
+            print('we have next page')
+            yield response.follow(next_forum_page, callback=self.parse)
 
     def parse_post(self, response):
         postContent = response.css("div.bbWrapper:first-child::text").get()
