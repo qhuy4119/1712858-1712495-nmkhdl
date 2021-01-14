f85c351f9c300a4a24b52b9ca9d7b8c3e80f4df4
Huy Nguyen Quang <quanghuy4119@gmail.com>
2021-01-14 21:06:03 +0700

    add data file

--- scraping/scraping/forumsList.py
+++ scraping/scraping/forumsList.py
@@ -1,4 +1,6 @@
 forums_to_crawl = [
-    "https://voz.vn/f/xe-cac-loai.77/",
-    "https://voz.vn/f/an-uong-du-lich.81/"
+    "https://voz.vn/f/phim-nhac-sach.65/",
+    "https://voz.vn/f/dien-thoai-di-dong.76/",
+    "https://voz.vn/f/4-banh.38/",
+    "https://voz.vn/f/do-dien-tu-thiet-bi-gia-dung.10/",
 ]
